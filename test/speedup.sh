26aee5deb1a3e9291e2fa8fedf4b222505030012
Erick <github@ericklavoie.com>
2017-04-13 11:02:08 -0400

    Added speedup measurement script

---
+++ test/speedup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+DIRNAME=$(dirname $0);
+DIR=$DIRNAME/results/speedup/$(date '+%Y-%m-%dT%H:%M:%S'); 
+COUNT_NB=100
+COUNT="$DIRNAME/count $COUNT_NB";
+PANDO="node $DIRNAME/../bin/index.js --stdin "$@;
+EXPECT_SQUARE=$DIRNAME/expect-square;
+SETTINGS="$DIR/speedup-settings.json"
+TIMING="$DIR/timings.txt"
+TIME="time"
+
+FUNCTION="$DIRNAME/../examples/square.js";
+DELEGATION_FACTOR=1;
+MAX_DEGREE=10;
+INTERVAL=3000;
+
+mkdir -p $DIR;
+echo 'Saving experiment settings in ' $DIR
+echo '{ "countNb":'$COUNT_NB', "maxDegree": '$MAX_DEGREE', "function": "'$FUNCTION'", "delegationFactor":'$DELEGATION_FACTOR' }' > $SETTINGS
+$COUNT | $PANDO $FUNCTION | $TIME $EXPECT_SQUARE 2> >(tee $TIMING >&2)
