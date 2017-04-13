684f35a689b5a5063fa6a42d1f362378131f4ce7
Erick <github@ericklavoie.com>
2017-04-13 11:01:51 -0400

    Fixed date bug

--- test/throughput.sh
+++ test/throughput.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 DIRNAME=$(dirname $0);
-DIR=$DIRNAME/results/$(date '+%Y-%M-%dT%H:%M:%S'); 
+DIR=$DIRNAME/results/throughput/$(date '+%Y-%m-%dT%H:%M:%S');
 COUNT=$DIRNAME/count;
-PANDO="node $DIRNAME/../bin/index.js --stdin --start-idle --headless --global-monitoring";
+PANDO="node $DIRNAME/../bin/index.js --stdin "$@;
 EXPECT_SQUARE=$DIRNAME/expect-square;
 MEASURE_THROUGHPUT=$DIRNAME/measure-throughput;
-LOG="tee $DIR/throughput.jsonl"
+LOG="tee $DIR/throughput-measurements.jsonl"
+SETTINGS="$DIR/throughput-settings.json"
 
 FUNCTION="$DIRNAME/../examples/square.js";
-DELEGATION_FACTOR=$2;
-MAX_DEGREE=$3;
+DELEGATION_FACTOR=1;
+MAX_DEGREE=10;
+INTERVAL=3000;
 
 mkdir -p $DIR;
-echo 'Saving experiment results in ' $DIR
-$COUNT | $PANDO $FUNCTION | $EXPECT_SQUARE | $MEASURE_THROUGHPUT 3000 | $LOG
-
-
-
+echo 'Saving experiment settings in ' $DIR
+echo '{ "maxDegree": '$MAX_DEGREE', "function": "'$FUNCTION'", "delegationFactor":'$DELEGATION_FACTOR' }' > $SETTINGS
+$COUNT | $PANDO $FUNCTION | $EXPECT_SQUARE | $MEASURE_THROUGHPUT $INTERVAL | $LOG
